04fb41ee57f457ab7431762c911741563bf80fc4
Joe Goeman <jgoeman86@gmail.com>
2022-01-16 19:29:35 -0600

    update MechaCarChallenge for deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,7 +7,6 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarMpg_table))
 
 
-
 # Deliverable 2
 suspension_coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 
@@ -17,3 +16,10 @@ total_summary <- suspension_coil_table %>%
 lot_summary <- suspension_coil_table %>%
   group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table by LOt number
+
+# Deliverable 3
+t.test(suspension_coil_table$PSI,mu=mean(1500))
+
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot1")$PSI, mu=mean(1500))
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot2")$PSI, mu=mean(1500))
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot3")$PSI, mu=mean(1500))
